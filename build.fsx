1790d214236af38598226e04dd2a5667310c1160
Rasmus Mikkelsen <r@smus.nu>
2015-03-22 15:20:33 +0100

    Create MSSQL packages

--- build.fsx
+++ build.fsx
@@ -12,7 +12,8 @@ let filePathUnitTestReport = dirReports + "/NUnit.xml"
 let fileListUnitTests = !! ("**/bin/" @@ buildMode @@ "/EventFlow*Tests.dll")
 let toolNUnit = "./Tools/NUnit.Runners/tools"
 let toolIlMerge = "./Tools/ilmerge/tools/ILMerge.exe"
-
+let nugetVersion = buildVersion + "-alpha"
+let nugetVersionDep = "["+nugetVersion+"]"
 
 Target "Clean" (fun _ ->
     CleanDirs [ dirPackages; dirReports ]
@@ -35,23 +36,67 @@ Target "UnitTest" (fun _ ->
     )
 
 Target "CreatePackageEventFlow" (fun _ ->
-    let eventFlowBinDir = "Source\\EventFlow\\bin\\" + buildMode + "\\"
-
+    let binDir = "Source\\EventFlow\\bin\\" + buildMode + "\\"
     let result = ExecProcess (fun info ->
-       info.Arguments <- "/targetplatform:v4 /internalize /allowDup /target:library /out:Source\\EventFlow\\bin\\EventFlow.dll " + eventFlowBinDir + "EventFlow.dll " + eventFlowBinDir + "Autofac.dll"
+       info.Arguments <- "/targetplatform:v4 /internalize /allowDup /target:library /out:Source\\EventFlow\\bin\\EventFlow.dll " + binDir + "EventFlow.dll " + binDir + "Autofac.dll"
        info.FileName <- toolIlMerge) (TimeSpan.FromMinutes 5.0)
-     
-    if result <> 0 then failwithf "ILMerge returned with a non-zero exit code"
-
+    if result <> 0 then failwithf "ILMerge of EventFlow returned with a non-zero exit code"
     NuGet (fun p -> 
         {p with
             OutputPath = dirPackages
             WorkingDir = "Source/EventFlow"
-            Version = buildVersion + "-alpha"
+            Version = nugetVersion
             Dependencies = [
                 "Newtonsoft.Json",  GetPackageVersion "./packages/" "Newtonsoft.Json"]
             Publish = false })
             "Source/EventFlow/EventFlow.nuspec"
+    )
+
+Target "CreatePackageEventFlowMsSql" (fun _ ->
+    let binDir = "Source\\EventFlow.MsSql\\bin\\" + buildMode + "\\"
+    let result = ExecProcess (fun info ->
+       info.Arguments <- "/targetplatform:v4 /internalize /allowDup /target:library /out:Source\\EventFlow.MsSql\\bin\\EventFlow.MsSql.dll " + binDir + "EventFlow.MsSql.dll " + binDir + "Dapper.dll "  + binDir + "DbUp.dll"
+       info.FileName <- toolIlMerge) (TimeSpan.FromMinutes 5.0)
+    if result <> 0 then failwithf "ILMerge of EventFlow.MsSql returned with a non-zero exit code"
+    NuGet (fun p -> 
+        {p with
+            OutputPath = dirPackages
+            WorkingDir = "Source/EventFlow.MsSql"
+            Version = nugetVersion
+            Dependencies = [
+                "EventFlow",  nugetVersionDep]
+            Publish = false })
+            "Source/EventFlow.MsSql/EventFlow.MsSql.nuspec"
+    )
+
+Target "CreatePackageEventFlowEventStoresMsSql" (fun _ ->
+    let binDir = "Source/EventFlow.EventStores.MsSql/bin/"
+    CopyFile binDir (binDir + buildMode + "/EventFlow.EventStores.MsSql.dll")
+    NuGet (fun p ->
+        {p with
+            OutputPath = dirPackages
+            WorkingDir = "Source/EventFlow.EventStores.MsSql"
+            Version = nugetVersion
+            Dependencies = [
+                "EventFlow",  nugetVersionDep
+                "EventFlow.MsSql",  nugetVersionDep]
+            Publish = false })
+            "Source/EventFlow.EventStores.MsSql/EventFlow.EventStores.MsSql.nuspec"
+    )
+
+Target "CreatePackageEventFlowReadStoresMsSql" (fun _ ->
+    let binDir = "Source/EventFlow.ReadStores.MsSql/bin/"
+    CopyFile binDir (binDir + buildMode + "/EventFlow.ReadStores.MsSql.dll")
+    NuGet (fun p ->
+        {p with
+            OutputPath = dirPackages
+            WorkingDir = "Source/EventFlow.ReadStores.MsSql"
+            Version = nugetVersion
+            Dependencies = [
+                "EventFlow",  nugetVersionDep
+                "EventFlow.MsSql",  nugetVersionDep]
+            Publish = false })
+            "Source/EventFlow.ReadStores.MsSql/EventFlow.ReadStores.MsSql.nuspec"
     )
 
 Target "Default" DoNothing
@@ -60,7 +105,9 @@ Target "Default" DoNothing
     ==> "BuildApp"
     ==> "UnitTest"
     ==> "CreatePackageEventFlow"
+    ==> "CreatePackageEventFlowMsSql"
+    ==> "CreatePackageEventFlowEventStoresMsSql"
+    ==> "CreatePackageEventFlowReadStoresMsSql"
     ==> "Default"
 
 RunTargetOrDefault "Default"
-
