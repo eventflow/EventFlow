f1d37c3fb27258b8d90d1806fdb4afa09c137872
Rasmus Mikkelsen <r@smus.nu>
2015-03-30 12:44:54 +0200

    Minor cleanup

--- Source/EventFlow/EventStores/EventStore.cs
+++ Source/EventFlow/EventStores/EventStore.cs
@@ -20,7 +20,6 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -81,10 +80,14 @@ namespace EventFlow.EventStores
             return domainEvents;
         }
 
-        protected abstract Task<IReadOnlyCollection<ICommittedDomainEvent>> CommitEventsAsync<TAggregate>(string id, IReadOnlyCollection<SerializedEvent> serializedEvents, CancellationToken cancellationToken)
+        protected abstract Task<IReadOnlyCollection<ICommittedDomainEvent>> CommitEventsAsync<TAggregate>(
+            string id, IReadOnlyCollection<SerializedEvent> serializedEvents,
+            CancellationToken cancellationToken)
             where TAggregate : IAggregateRoot;
 
-        protected abstract Task<IReadOnlyCollection<ICommittedDomainEvent>> LoadCommittedEventsAsync(string id, CancellationToken cancellationToken);
+        protected abstract Task<IReadOnlyCollection<ICommittedDomainEvent>> LoadCommittedEventsAsync(
+            string id,
+            CancellationToken cancellationToken);
 
         public virtual async Task<IReadOnlyCollection<IDomainEvent>> LoadEventsAsync(string id, CancellationToken cancellationToken)
         {
@@ -95,7 +98,9 @@ namespace EventFlow.EventStores
             return domainEvents;
         }
 
-        public virtual async Task<TAggregate> LoadAggregateAsync<TAggregate>(string id, CancellationToken cancellationToken)
+        public virtual async Task<TAggregate> LoadAggregateAsync<TAggregate>(
+            string id,
+            CancellationToken cancellationToken)
             where TAggregate : IAggregateRoot
         {
             var aggregateType = typeof(TAggregate);
