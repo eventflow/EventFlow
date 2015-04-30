7ab8e8485de0ecd2b5d41ae46af8c7bfe0a8facd
Rasmus Mikkelsen <r@smus.nu>
2015-04-30 09:22:27 +0200

    We haven't release version 1.0 yet, so remove the pre-release flag from NuGet packages as its redundant

--- Source/SolutionInfo.cs
+++ Source/SolutionInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 [assembly: AssemblyVersionAttribute("0.0.1")]
-[assembly: AssemblyInformationalVersionAttribute("0.0.1-alpha")]
+[assembly: AssemblyInformationalVersionAttribute("0.0.1")]
 [assembly: AssemblyFileVersionAttribute("0.0.1")]
 namespace System {
     internal static class AssemblyVersionInformation {
