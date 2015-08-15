e8e57710b692cddf6c9fe75d7393627e64513181
Rasmus Mikkelsen <r@smus.nu>
2015-08-15 16:15:06 +0200

    Create a base class that developers can use to create state objects for aggregates

---
+++ Source/EventFlow/Aggregates/EventAppliers/EmitEventApplier.cs
@@ -0,0 +1,87 @@
+﻿// The MIT License (MIT)
+//
+// Copyright (c) 2015 Rasmus Mikkelsen
+// https://github.com/rasmus/EventFlow
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using EventFlow.Extensions;
+
+namespace EventFlow.Aggregates.EventAppliers
+{
+    public abstract class EmitEventApplier<TEventApplier, TAggregate, TIdentity> : IEventApplier<TAggregate, TIdentity>
+        where TEventApplier : class, IEventApplier<TAggregate, TIdentity>
+        where TAggregate : IAggregateRoot<TIdentity>
+        where TIdentity : IIdentity
+    {
+        private static readonly Dictionary<Type, Action<TEventApplier, IAggregateEvent<TAggregate, TIdentity>>> ApplyMethods; 
+
+        static EmitEventApplier()
+        {
+            var aggregateEventType = typeof (IAggregateEvent<TAggregate, TIdentity>);
+
+            ApplyMethods = typeof (TEventApplier)
+                .GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(mi =>
+                    {
+                        if (mi.Name != "Apply") return false;
+                        var parameters = mi.GetParameters();
+                        return
+                            parameters.Length == 1 &&
+                            aggregateEventType.IsAssignableFrom(parameters[0].ParameterType);
+                    })
+                .ToDictionary(
+                    mi => mi.GetParameters()[0].ParameterType,
+                    mi => (Action<TEventApplier, IAggregateEvent<TAggregate, TIdentity>>) ((ea, e) => mi.Invoke(ea, new []{ e } )));
+        }
+
+        protected EmitEventApplier()
+        {
+            var me = this as TEventApplier;
+            if (me == null)
+            {
+                throw new InvalidOperationException(string.Format(
+                    "Event applier of type '{0}' has a wrong generic argument '{1}'",
+                    GetType().PrettyPrint(),
+                    typeof(TEventApplier).PrettyPrint()));
+            }
+        }
+
+        public void Apply(
+            TAggregate aggregate,
+            IAggregateEvent<TAggregate, TIdentity> aggregateEvent)
+        {
+            var aggregateEventType = aggregateEvent.GetType();
+            Action<TEventApplier, IAggregateEvent<TAggregate, TIdentity>> applier;
+
+            if (!ApplyMethods.TryGetValue(aggregateEventType, out applier))
+            {
+                throw new ArgumentException(string.Format(
+                    "Event applier '{0}' does not implement 'Apply' method for event '{1}'",
+                    GetType().PrettyPrint(),
+                    aggregateEventType.PrettyPrint()));
+            }
+
+            applier((TEventApplier) (object) this, aggregateEvent);
+        }
+    }
+}
