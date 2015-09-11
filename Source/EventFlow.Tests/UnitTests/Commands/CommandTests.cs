5fa320db1f49c1139152611f39fad6609c61fffb
Rasmus Mikkelsen <r@smus.nu>
2015-09-11 06:57:21 +0200

    Illustrate the serialization problem

---
+++ Source/EventFlow.Tests/UnitTests/Commands/CommandTests.cs
@@ -0,0 +1,66 @@
+﻿// The MIT License (MIT)
+//
+// Copyright (c) 2015 Rasmus Mikkelsen
+// https://github.com/rasmus/EventFlow
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using EventFlow.Commands;
+using EventFlow.Core;
+using EventFlow.TestHelpers;
+using EventFlow.TestHelpers.Aggregates.Test;
+using FluentAssertions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace EventFlow.Tests.UnitTests.Commands
+{
+    public class CommandTests : Test
+    {
+        public class CriticalCommand : Command<TestAggregate, TestId>
+        {
+            public string CriticalData { get; }
+
+            public CriticalCommand(TestId aggregateId, string criticalData) : base(aggregateId)
+            {
+                CriticalData = criticalData;
+            }
+
+            public CriticalCommand(TestId aggregateId, ISourceId sourceId, string criticalData) : base(aggregateId, sourceId)
+            {
+                CriticalData = criticalData;
+            }
+        }
+
+        [Test]
+        public void SerializeDeserialize()
+        {
+            // Arrange
+            var criticalCommand = A<CriticalCommand>();
+
+            // Act
+            var json = JsonConvert.SerializeObject(criticalCommand);
+            var deserialized = JsonConvert.DeserializeObject<CriticalCommand>(json);
+
+            // Assert
+            deserialized.CriticalData.Should().Be(criticalCommand.CriticalData);
+            deserialized.SourceId.Should().Be(criticalCommand.SourceId);
+            deserialized.AggregateId.Should().Be(criticalCommand.AggregateId);
+        }
+    }
+}
