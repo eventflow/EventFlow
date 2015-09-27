c9d2b2af9ce8e91e374737e310f4cb26c20da73c
Rasmus Mikkelsen <r@smus.nu>
2015-09-27 19:14:23 +0200

    Expose method on interface

--- Source/EventFlow/Configuration/Decorators/IDecoratorService.cs
+++ Source/EventFlow/Configuration/Decorators/IDecoratorService.cs
@@ -20,10 +20,13 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+using System;
+
 namespace EventFlow.Configuration.Decorators
 {
     public interface IDecoratorService
     {
         TService Decorate<TService>(TService implementation, IResolverContext resolverContext);
+        object Decorate(Type serviceType, object implementation, IResolverContext resolverContext);
     }
 }
