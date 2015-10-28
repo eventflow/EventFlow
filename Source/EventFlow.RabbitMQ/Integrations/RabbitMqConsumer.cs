280f18fba297ac6fefb1ef43f32f6829ca60b2c6
Rasmus Mikkelsen <r@smus.nu>
2015-10-28 21:10:31 +0100

    Forgot one file header

--- Source/EventFlow.RabbitMQ/Integrations/RabbitMqConsumer.cs
+++ Source/EventFlow.RabbitMQ/Integrations/RabbitMqConsumer.cs
@@ -1,6 +1,7 @@
 ﻿// The MIT License (MIT)
 //
 // Copyright (c) 2015 Rasmus Mikkelsen
+// Copyright (c) 2015 eBay Software Foundation
 // https://github.com/rasmus/EventFlow
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
