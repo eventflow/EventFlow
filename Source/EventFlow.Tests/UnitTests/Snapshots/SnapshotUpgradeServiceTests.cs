f4b5802d541ae2fddb2aef32396e95a0e204315c
Rasmus Mikkelsen <r@smus.nu>
2016-04-09 00:14:52 +0200

    Started on tests for version type upgraders

---
+++ Source/EventFlow.Tests/UnitTests/Snapshots/SnapshotUpgradeServiceTests.cs
@@ -0,0 +1,90 @@
+﻿// The MIT License (MIT)
+// 
+// Copyright (c) 2015-2016 Rasmus Mikkelsen
+// Copyright (c) 2015-2016 eBay Software Foundation
+// https://github.com/rasmus/EventFlow
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System.Linq;
+using System.Threading;
+using EventFlow.Configuration;
+using EventFlow.Logs;
+using EventFlow.Snapshots;
+using EventFlow.TestHelpers;
+using EventFlow.TestHelpers.Aggregates.Snapshots;
+using EventFlow.TestHelpers.Aggregates.Snapshots.Upgraders;
+using EventFlow.TestHelpers.Aggregates.ValueObjects;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace EventFlow.Tests.UnitTests.Snapshots
+{
+    public class SnapshotUpgradeServiceTests : TestsFor<SnapshotUpgradeService>
+    {
+        private Mock<IResolver> _resolverMock;
+        private ISnapshotDefinitionService _snapshotDefinitionService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _resolverMock = InjectMock<IResolver>();
+            _snapshotDefinitionService = Inject<ISnapshotDefinitionService>(new SnapshotDefinitionService(A<ILog>()));
+        }
+
+        [Test]
+        public void UpgradeAsync_UpgradesSnapshot()
+        {
+            // Arrange
+            var pingIds = Many<PingId>();
+            Arrange_All_Upgraders();
+            _snapshotDefinitionService.Load(typeof(ThingySnapshotV1), typeof(ThingySnapshotV2), typeof(ThingySnapshot));
+
+            // Act
+            var snapshot = Sut.UpgradeAsync(new ThingySnapshotV1(pingIds), CancellationToken.None).Result;
+            snapshot.Should().BeOfType<ThingySnapshot>();
+            var thingySnapshot = (ThingySnapshot) snapshot;
+            thingySnapshot.PingsReceived.Should().BeEquivalentTo(pingIds);
+            thingySnapshot.PreviousVersions.Should().BeEquivalentTo(new[] {ThingySnapshotVersion.Version1, ThingySnapshotVersion.Version2});
+        }
+
+        private void Arrange_All_Upgraders()
+        {
+            Arrange_Upgraders(
+                new ThingySnapshotV1ToV2Upgrader(),
+                new ThingySnapshotV2ToV3Upgrader());
+        }
+
+        private void Arrange_Upgraders(params object[] snapshotUpgraders)
+        {
+            foreach (var snapshotUpgrader in snapshotUpgraders)
+            {
+                var snapshotUpgraderInterfaceType = snapshotUpgrader
+                    .GetType()
+                    .GetInterfaces()
+                    .Single(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof (ISnapshotUpgrader<,>));
+
+                _resolverMock
+                    .Setup(r => r.Resolve(snapshotUpgraderInterfaceType))
+                    .Returns(snapshotUpgrader);
+            }
+        }
+    }
+}
