e3dea2c29374dff1ec201c08cc2850f6dc949779
Rasmus Mikkelsen <r@smus.nu>
2016-04-16 22:51:19 +0200

    First stab at MSSQL snapshot store

---
+++ Source/EventFlow.MsSql/SnapshotStores/Scripts/0001 - Create EventFlowSnapshots.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[EventFlowSnapshots](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[AggregateId] [nvarchar](128) NOT NULL,
+	[AggregateName] [nvarchar](128) NOT NULL,
+	[AggregateSequenceNumber] [int] NOT NULL,
+	[Data] [nvarchar](MAX) NOT NULL,
+	[Metadata] [nvarchar](MAX) NOT NULL,
+	CONSTRAINT [PK_EventFlow] PRIMARY KEY CLUSTERED
+	(
+		[Id] ASC
+	)
+)
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_EventFlow_AggregateId_AggregateSequenceNumber] ON [dbo].[EventFlow]
+(
+	[AggregateId] ASC,
+	[AggregateSequenceNumber] ASC
+)
