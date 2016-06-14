d17a86f18a88aeb14e789ee72d56ed10786f5976
Rasmus Mikkelsen <r@smus.nu>
2016-06-14 20:47:10 +0200

    Update build script

--- build.fsx
+++ build.fsx
@@ -42,7 +42,7 @@ Target "Test" (fun _ ->
         (fun p -> { 
             p with 
                 ExePath = "./packages/test/OpenCover/tools/OpenCover.Console.exe"
-                TestRunnerExePath = "./packages/build/NUnit.Runners/tools/nunit-console.exe"
+                TestRunnerExePath = "./packages/build/NUnit.ConsoleRunner/tools/nunit3-console.exe"
                 Output = dirReports + "/opencover-results.xml"
                 TimeOut = TimeSpan.FromMinutes 30.0;
                 Register = RegisterUser
