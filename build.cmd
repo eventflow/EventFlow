dca22dc2fd18bbcd6d3d7f5375b64e3dfa5e2c40
Rasmus Mikkelsen <r@smus.nu>
2016-06-28 22:35:51 +0200

    Use proper file name

--- build.cmd
+++ build.cmd
@@ -10,6 +10,6 @@ IF %errorlevel% NEQ 0 (
 	exit /b %errorlevel%
 )
 
-"packages\build\Cake\Cake.exe" build.cs -nugetApikey=%NUGET_APIKEY% -buildVersion=%APPVEYOR_BUILD_VERSION%
+"packages\build\Cake\Cake.exe" build.cake -nugetApikey=%NUGET_APIKEY% -buildVersion=%APPVEYOR_BUILD_VERSION%
 
 exit /b %errorlevel%
