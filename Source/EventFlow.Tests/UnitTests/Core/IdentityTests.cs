4707eec4cf52c2948e928a6a17aaea72fb04ff0b
Rasmus Mikkelsen <r@smus.nu>
2016-08-18 00:14:22 +0200

    Add a few more tests

--- Source/EventFlow.Tests/UnitTests/Core/IdentityTests.cs
+++ Source/EventFlow.Tests/UnitTests/Core/IdentityTests.cs
@@ -33,7 +33,7 @@ namespace EventFlow.Tests.UnitTests.Core
     public class IdentityTests
     {
         [Test]
-        public void NewDeterministic()
+        public void NewDeterministic_ReturnsKnownResult()
         {
             // Arrange
             var namespaceId = Guid.Parse("769077C6-F84D-46E3-AD2E-828A576AAAF3");
@@ -74,17 +74,38 @@ namespace EventFlow.Tests.UnitTests.Core
         }
 
         [Test]
-        public void NewIsValid()
+        public void New_IsValid()
         {
             // Arrange
             var testId = ThingyId.New;
 
             // Assert
-            ThingyId.IsValid(testId.Value).Should().BeTrue();
+            ThingyId.IsValid(testId.Value).Should().BeTrue(testId.Value);
+        }
+
+        [Test]
+        public void NewComb_IsValid()
+        {
+            // Arrange
+            var testId = ThingyId.NewComb();
+
+            // Assert
+            ThingyId.IsValid(testId.Value).Should().BeTrue(testId.Value);
+        }
+
+        [Test]
+        public void NewDeterministic_IsValid()
+        {
+            // Arrange
+            var testId = ThingyId.NewDeterministic(Guid.NewGuid(), Guid.NewGuid().ToString());
+
+            // Assert
+            ThingyId.IsValid(testId.Value).Should().BeTrue(testId.Value);
         }
 
         [TestCase("da7ab6b1-c513-581f-a1a0-7cdf17109deb")]
         [TestCase("thingy-769077C6-F84D-46E3-AD2E-828A576AAAF3")]
+        [TestCase("thingy-pppppppp-pppp-pppp-pppp-pppppppppppp")]
         [TestCase("funny-da7ab6b1-c513-581f-a1a0-7cdf17109deb")]
         [TestCase(null)]
         [TestCase("")]
