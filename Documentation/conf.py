f4bd7a07066f5112c68f6d7c28a6e7b48bfecd28
Rasmus Mikkelsen <r@smus.nu>
2016-10-06 19:49:33 +0200

    Updated

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -29,7 +29,9 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'EventFlow'
-copyright = u'2016, Rasmus Mikkelsen'
+copyright = u'2015-2016, Rasmus Mikkelsen'
 author = u'Rasmus Mikkelsen'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -119,7 +121,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
