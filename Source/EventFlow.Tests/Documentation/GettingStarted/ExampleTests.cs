a655ce2d95950a290826e72f4dbeae06c4e74049
Rasmus Mikkelsen <r@smus.nu>
2016-11-09 19:35:43 +0100

    Moved example classes into separate files

--- Source/EventFlow.Tests/Documentation/GettingStarted/ExampleTests.cs
+++ Source/EventFlow.Tests/Documentation/GettingStarted/ExampleTests.cs
@@ -24,21 +24,16 @@
 
 using System.Threading;
 using System.Threading.Tasks;
-using EventFlow.Aggregates;
-using EventFlow.Commands;
-using EventFlow.Core;
-using EventFlow.Exceptions;
 using EventFlow.Extensions;
 using EventFlow.Queries;
-using EventFlow.ReadStores;
 using EventFlow.TestHelpers;
 using FluentAssertions;
 using NUnit.Framework;
 
-namespace EventFlow.Tests.IntegrationTests
+namespace EventFlow.Tests.Documentation.GettingStarted
 {
     [Category(Categories.Integration)]
-    public class CompleteExampleTests
+    public class ExampleTests
     {
         [Test]
         public async Task Example()
@@ -72,90 +67,6 @@ namespace EventFlow.Tests.IntegrationTests
 
                 // Verify that the read model has the expected magic number
                 simpleReadModel.MagicNumber.Should().Be(42);
-            }
-        }
-
-        // Represents the aggregate identity (ID)
-        public class ExampleId : Identity<ExampleId>
-        {
-            public ExampleId(string value) : base(value) { }
-        }
-
-        // The aggregate root
-        public class ExampleAggrenate : AggregateRoot<ExampleAggrenate, ExampleId>,
-            IEmit<ExampleEvent>
-        {
-            private int? _magicNumber;
-
-            public ExampleAggrenate(ExampleId id) : base(id) { }
-
-            // Method invoked by our command
-            public void SetMagicNumer(int magicNumber)
-            {
-                if (_magicNumber.HasValue)
-                    throw DomainError.With("Magic number already set");
-
-                Emit(new ExampleEvent(magicNumber));
-            }
-
-            // We apply the event as part of the event sourcing system. EventFlow
-            // provides several different methods for doing this, e.g. state objects,
-            // the Apply method is merely the simplest
-            public void Apply(ExampleEvent aggregateEvent)
-            {
-                _magicNumber = aggregateEvent.MagicNumber;
-            }
-        }
-
-        // A basic event containing some information
-        public class ExampleEvent : AggregateEvent<ExampleAggrenate, ExampleId>
-        {
-            public ExampleEvent(int magicNumber)
-            {
-                MagicNumber = magicNumber;
-            }
-
-            public int MagicNumber { get; }
-        }
-
-        // Command for update magic number
-        public class ExampleCommand : Command<ExampleAggrenate, ExampleId>
-        {
-            public ExampleCommand(
-                ExampleId aggregateId,
-                int magicNumber)
-                : base(aggregateId)
-            {
-                MagicNumber = magicNumber;
-            }
-
-            public int MagicNumber { get; }
-        }
-
-        // Command handler for our command
-        public class ExampleCommandHandler : CommandHandler<ExampleAggrenate, ExampleId, ExampleCommand>
-        {
-            public override Task ExecuteAsync(
-                ExampleAggrenate aggregate, 
-                ExampleCommand command,
-                CancellationToken cancellationToken)
-            {
-                aggregate.SetMagicNumer(command.MagicNumber);
-                return Task.FromResult(0);
-            }
-        }
-
-        // Read model for our aggregate
-        public class ExampleReadModel : IReadModel,
-            IAmReadModelFor<ExampleAggrenate, ExampleId, ExampleEvent>
-        {
-            public int MagicNumber { get; private set; }
-
-            public void Apply(
-                IReadModelContext context,
-                IDomainEvent<ExampleAggrenate, ExampleId, ExampleEvent> domainEvent)
-            {
-                MagicNumber = domainEvent.AggregateEvent.MagicNumber;
             }
         }
     }
