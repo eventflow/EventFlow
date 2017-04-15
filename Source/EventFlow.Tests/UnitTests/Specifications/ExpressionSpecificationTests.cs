898c18e98d5ae5fa89bc0245ce4d704d93cc28dc
Rasmus Mikkelsen <r@smus.nu>
2017-04-15 16:52:43 +0200

    Added missing category

--- Source/EventFlow.Tests/UnitTests/Specifications/ExpressionSpecificationTests.cs
+++ Source/EventFlow.Tests/UnitTests/Specifications/ExpressionSpecificationTests.cs
@@ -22,11 +22,13 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 using EventFlow.Provided.Specifications;
+using EventFlow.TestHelpers;
 using FluentAssertions;
 using NUnit.Framework;
 
 namespace EventFlow.Tests.UnitTests.Specifications
 {
+    [Category(Categories.Unit)]
     public class ExpressionSpecificationTests
     {
         [Test]
