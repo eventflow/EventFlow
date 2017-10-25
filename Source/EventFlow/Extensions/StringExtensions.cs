4cab20b73abad475962164e8321c182af3fcbac1
Frank Ebersoll <frank.ebersoll@hauertmann.net>
2017-10-25 14:04:48 +0200

    Fixed #399: Made static Sha256 instance transient

--- Source/EventFlow/Extensions/StringExtensions.cs
+++ Source/EventFlow/Extensions/StringExtensions.cs
@@ -31,7 +31,6 @@ namespace EventFlow.Extensions
     public static class StringExtensions
     {
         private static readonly Regex RegexToSlug = new Regex("(?<=.)([A-Z])", RegexOptions.Compiled);
-        private static readonly SHA256 Sha256Managed = SHA256.Create();
 
         public static string ToSlug(this string str)
         {
@@ -41,10 +40,13 @@ namespace EventFlow.Extensions
         public static string ToSha256(this string str)
         {
             var bytes = Encoding.UTF8.GetBytes(str);
-            var hash = Sha256Managed.ComputeHash(bytes);
-            return hash
-                .Aggregate(new StringBuilder(), (sb, b) => sb.Append($"{b:x2}"))
-                .ToString();
+            using (var sha256 = SHA256.Create())
+            {
+                var hash = sha256.ComputeHash(bytes);
+                return hash
+                    .Aggregate(new StringBuilder(), (sb, b) => sb.Append($"{b:x2}"))
+                    .ToString();
+            }
         }
     }
 }
