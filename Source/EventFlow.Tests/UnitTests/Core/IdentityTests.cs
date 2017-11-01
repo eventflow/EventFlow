e601612ee1f95fbeb1059520c8c8229620a223e8
Frank Ebersoll <frank.ebersoll@hauertmann.net>
2017-11-01 20:43:50 +0100

    Identity prefix validation with dash

--- Source/EventFlow.Tests/UnitTests/Core/IdentityTests.cs
+++ Source/EventFlow.Tests/UnitTests/Core/IdentityTests.cs
@@ -118,6 +118,7 @@ namespace EventFlow.Tests.UnitTests.Core
         }
 
         [TestCase("da7ab6b1-c513-581f-a1a0-7cdf17109deb")]
+        [TestCase("thingyid-da7ab6b1-c513-581f-a1a0-7cdf17109deb")]
         [TestCase("thingy-769077C6-F84D-46E3-AD2E-828A576AAAF3")]
         [TestCase("thingy-pppppppp-pppp-pppp-pppp-pppppppppppp")]
         [TestCase("funny-da7ab6b1-c513-581f-a1a0-7cdf17109deb")]
