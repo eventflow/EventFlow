2bc31ad2763f6ed34a3cb426b13a62eaffc73489
Rasmus Mikkelsen <r@smus.nu>
2018-06-09 12:27:46 +0200

    Cleanup

--- Source/EventFlow/ReadStores/SingleAggregateReadStoreManager.cs
+++ Source/EventFlow/ReadStores/SingleAggregateReadStoreManager.cs
@@ -21,6 +21,7 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -64,7 +65,9 @@ namespace EventFlow.ReadStores
 
             await ReadModelDomainEventApplier.UpdateReadModelAsync(readModel, domainEvents, readModelContext, cancellationToken).ConfigureAwait(false);
 
-            var readModelVersion = domainEvents.Max(e => e.AggregateSequenceNumber);
+            var readModelVersion = Math.Max(
+                domainEvents.Max(e => e.AggregateSequenceNumber),
+                readModelEnvelope.Version.GetValueOrDefault());
 
             return ReadModelEnvelope<TReadModel>.With(readModelEnvelope.ReadModelId, readModel, readModelVersion);
         }
