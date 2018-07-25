1b27f4c1017b346c322fbfca92b7d941d5fbc03d
idubnori <i.dub.nori@gmail.com>
2018-07-26 00:27:58 +0900

    Implemented PostgreSql tests of EntityFramework.

---
+++ Source/EventFlow.EntityFramework.Tests/PostgreSql/EfPostgreSqlReadStoreTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using EventFlow.Configuration;
+using EventFlow.EntityFramework.Tests.Model;
+using EventFlow.Extensions;
+using EventFlow.PostgreSql.Connections;
+using EventFlow.PostgreSql.Extensions;
+using EventFlow.PostgreSql.TestsHelpers;
+using EventFlow.TestHelpers;
+using EventFlow.TestHelpers.Suites;
+using NUnit.Framework;
+
+namespace EventFlow.EntityFramework.Tests.PostgreSql
+{
+    [Category(Categories.Integration)]
+    public class EfPostgreSqlReadStoreTests : TestSuiteForReadModelStore
+    {
+        private IPostgreSqlDatabase _testDatabase;
+
+        protected override Type ReadModelType => typeof(ThingyReadModelEntity);
+
+        protected override IRootResolver CreateRootResolver(IEventFlowOptions eventFlowOptions)
+        {
+            _testDatabase = PostgreSqlHelpz.CreateDatabase("eventflow");
+
+            return eventFlowOptions
+                .ConfigurePostgreSql(PostgreSqlConfiguration.New.SetConnectionString(_testDatabase.ConnectionString.Value))
+                .ConfigureForReadStoreTest<PostgreSqlDbContextProvider>()
+                .CreateResolver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _testDatabase.DisposeSafe("Failed to delete database");
+        }
+    }
+}
---
+++ Source/EventFlow.EntityFramework.Tests/PostgreSql/PostgreSqlDbContextProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using EventFlow.EntityFramework.Tests.Model;
+using EventFlow.PostgreSql.Connections;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventFlow.EntityFramework.Tests.PostgreSql
+{
+    public class PostgreSqlDbContextProvider : IDbContextProvider<TestDbContext>, IDisposable
+    {
+        private readonly string _connectionString;
+
+        public PostgreSqlDbContextProvider(IPostgreSqlConfiguration postgreSqlConfiguration)
+        {
+            _connectionString = postgreSqlConfiguration.ConnectionString;
+        }
+
+        public TestDbContext CreateContext()
+        {
+            var options = new DbContextOptionsBuilder<TestDbContext>()
+                .UseNpgsql(_connectionString)
+                .Options;
+            var context = new TestDbContext(options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
