3fb348af0f24b40b73f022df0c687f79e7e7ed03
idubnori <i.dub.nori@gmail.com>
2018-08-27 14:53:07 +0900

    Revert "Removed in-memory tests in EntityFramework."
    This reverts commit 7fac7e9b41d30fd4630ee9b5d360f5fe982a3746.

---
+++ Source/EventFlow.EntityFramework.Tests/InMemory/EfInMemoryEventStoreTests.cs
@@ -0,0 +1,20 @@
+﻿using EventFlow.Configuration;
+using EventFlow.EntityFramework.Extensions;
+using EventFlow.TestHelpers;
+using EventFlow.TestHelpers.Suites;
+using NUnit.Framework;
+
+namespace EventFlow.EntityFramework.Tests.InMemory
+{
+    [Category(Categories.Integration)]
+    public class EfInMemoryEventStoreTests : TestSuiteForEventStore
+    {
+        protected override IRootResolver CreateRootResolver(IEventFlowOptions eventFlowOptions)
+        {
+            return eventFlowOptions
+                .ConfigureEntityFramework()
+                .ConfigureForEventStoreTest<InMemoryDbContextProvider>()
+                .CreateResolver();
+        }
+    }
+}
