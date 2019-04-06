e63a19f7028f7e1d47c398f241ec7ba5f0fe1cd4
Michal Bičan <bicanm@gmail.com>
2019-04-06 15:45:16 +0200

    #599 Start creating a test

---
+++ Source/EventFlow.Tests/UnitTests/Sagas/AggregateSagas/AggregateSagaTests.cs
@@ -0,0 +1,21 @@
+﻿using EventFlow.Sagas.AggregateSagas;
+using EventFlow.TestHelpers;
+using EventFlow.TestHelpers.Aggregates.Sagas;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventFlow.Tests.UnitTests.Sagas.AggregateSagas
+{
+    [Category(Categories.Unit)]
+    public class AggregateSagaTests: TestsFor<ThingySaga>
+    {
+        [Test]
+        public async Task AggregateSaga_Publish()
+        {
+        }
+    }
+}
