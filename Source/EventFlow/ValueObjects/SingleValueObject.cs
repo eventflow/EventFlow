5427151e703d8d322ac5a4397e0bd5494f13b662
Rasmus Mikkelsen <r@smus.nu>
2019-09-02 20:39:52 +0200

    Remove dead code

--- Source/EventFlow/ValueObjects/SingleValueObject.cs
+++ Source/EventFlow/ValueObjects/SingleValueObject.cs
@@ -28,30 +28,6 @@ using EventFlow.Extensions;
 
 namespace EventFlow.ValueObjects
 {
-    public class NullableComparer<T> : IComparer<Nullable<T>>
-        where T : struct, IComparable<T>
-    {
-
-        public int Compare(Nullable<T> x, Nullable<T> y)
-        {
-            // Compare nulls acording MSDN specification
-
-            //Two nulls are equal
-            if (!x.HasValue && !y.HasValue)
-                return 0;
-
-            //Any object is greater than null
-            if (x.HasValue && !y.HasValue) 
-                return 1;
-
-            if (y.HasValue && !x.HasValue)
-                return -1;
-
-            //Otherwise compare the two values
-            return x.Value.CompareTo(y.Value);
-        }
-    }
-
     public abstract class SingleValueObject<T> : ValueObject, IComparable, ISingleValueObject
         where T : IComparable
     {
