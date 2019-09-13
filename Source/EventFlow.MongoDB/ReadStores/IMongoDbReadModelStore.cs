7233105f13c1ca7969b3c7f2c607029640d1d1df
Fill Scheers <fill.scheers@innovationcrib.be>
2019-09-13 13:22:29 +0200

    mongoDB as IQueryable

--- Source/EventFlow.MongoDB/ReadStores/IMongoDbReadModelStore.cs
+++ Source/EventFlow.MongoDB/ReadStores/IMongoDbReadModelStore.cs
@@ -4,15 +4,18 @@ using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using MongoDB.Driver;
+using System.Linq;
 
 namespace EventFlow.MongoDB.ReadStores
 {
-	public interface IMongoDbReadModelStore<TReadModel> : IReadModelStore<TReadModel>
-		where TReadModel : class, IReadModel, new()
-	{
-		Task<IAsyncCursor<TReadModel>> FindAsync(
-			Expression<Func<TReadModel, bool>> filter,
-			FindOptions<TReadModel, TReadModel> options = null,
-			CancellationToken cancellationToken = default(CancellationToken));
-	}
+    public interface IMongoDbReadModelStore<TReadModel> : IReadModelStore<TReadModel>
+        where TReadModel : class, IReadModel, new()
+    {
+        Task<IAsyncCursor<TReadModel>> FindAsync(
+            Expression<Func<TReadModel, bool>> filter,
+            FindOptions<TReadModel, TReadModel> options = null,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        IQueryable<TReadModel> AsQueryable();
+    }
 }
