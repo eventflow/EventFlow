f0d462f12188247fcbaedbecd9fa45e7660d0a5c
Emanuele Curati <ema@outlook.com>
2019-10-10 15:16:59 +0200

    Fix typo

--- up_integration-test-env.ps1
+++ up_integration-test-env.ps1
@@ -12,19 +12,16 @@ sal curl (Join-Path $env:ChocolateyInstall "bin\curl.exe") -O AllScope
 
 # Set connection url to environment variable
 # RabbitMQ
-$rabbitmq_ip = Get-Container-Ip rabbitmq-ef
 $env:RABBITMQ_URL = "amqp://guest:guest@$localhost:5672"
 # Elasticsearch
-$elasticsearch_ip = Get-Container-Ip elasticsearch-ef
 $env:ELASTICSEARCH_URL = "http://localhost:9200"
 # Event Store
-$eventstore_ip = Get-Container-Ip eventstore-ef
 $env:EVENTSTORE_URL = "tcp://admin:changeit@$localhost:1113"
 
 # Helth check
 # Event Store
-curl --connect-timeout 60 --retry 5 -sL "http://${eventstore_ip}:2113"
+curl --connect-timeout 60 --retry 5 -sL "http://localhost:2113"
 # Elasticsearch
-curl --connect-timeout 60 --retry 5 -sL "http://${elasticsearch_ip}:9200"
+curl --connect-timeout 60 --retry 5 -sL "http://localhost:9200"
 # RabbitMQ
-curl --connect-timeout 60 --retry 5 -sL "http://${rabbitmq_ip}:15672"
+curl --connect-timeout 60 --retry 5 -sL "http://localhost:15672"
