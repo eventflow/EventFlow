3583d98648cebd6bd30726618890eedcccef52a0
Emanuele Curati <ema@outlook.com>
2019-10-16 11:23:38 +0200

    Remove empty ctor requirement also on mongo readmodels

--- Source/EventFlow.MongoDB/ReadStores/IMongoDbReadModelStore.cs
+++ Source/EventFlow.MongoDB/ReadStores/IMongoDbReadModelStore.cs
@@ -8,7 +8,7 @@ using MongoDB.Driver;
 namespace EventFlow.MongoDB.ReadStores
 {
 	public interface IMongoDbReadModelStore<TReadModel> : IReadModelStore<TReadModel>
-		where TReadModel : class, IReadModel, new()
+		where TReadModel : class, IReadModel
 	{
 		Task<IAsyncCursor<TReadModel>> FindAsync(
 			Expression<Func<TReadModel, bool>> filter,
