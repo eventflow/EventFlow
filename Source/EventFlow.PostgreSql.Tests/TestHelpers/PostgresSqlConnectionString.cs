6afd71604bbfc4ed878e4fc827e77c6b94b1aeaf
Rasmus Mikkelsen <r@smus.nu>
2019-12-09 19:57:03 +0100

    Correct license header in accordance with CLA

--- Source/EventFlow.PostgreSql.Tests/TestHelpers/PostgresSqlConnectionString.cs
+++ Source/EventFlow.PostgreSql.Tests/TestHelpers/PostgresSqlConnectionString.cs
@@ -1,6 +1,6 @@
 ﻿// The MIT License (MIT)
 //
-// Copyright (c) 2015-2018 Rida Messaoudene
+// Copyright (c) 2015-2018 Rasmus Mikkelsen
 // https://github.com/rasmus/Helpz
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
