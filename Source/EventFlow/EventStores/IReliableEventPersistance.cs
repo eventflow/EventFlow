8d43a862ea9d6f0801f8a8d24ff34914f0d941e3
Michal Bičan <michal@efaut.com>
2020-01-09 22:26:07 +0100

    create IReliableEventPersistance

---
+++ Source/EventFlow/EventStores/IReliableEventPersistance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using EventFlow.Core;
+
+namespace EventFlow.EventStores
+{
+    public interface IReliableEventPersistance: IEventPersistence
+    {
+        Task MarkEventsDeliveredAsync(IIdentity id, IReadOnlyCollection<SerializedEvent> serializedEvents, CancellationToken cancellationToken);
+
+        Task<AllCommittedEventsPage> LoadAllUndeliveredEvents(GlobalPosition globalPosition, int pageSize, CancellationToken cancellationToken);
+
+        Task<IReadOnlyCollection<ICommittedDomainEvent>> LoadUndeliveredEventsAsync(IIdentity id, int fromEventSequenceNumber, CancellationToken cancellationToken);
+    }
+}
