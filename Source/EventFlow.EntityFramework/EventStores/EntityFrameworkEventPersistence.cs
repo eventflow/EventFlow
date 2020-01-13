56574398522ebf6be255bf762f966e606a0b60ce
Michal Bičan <michal@efaut.com>
2020-01-13 21:46:55 +0100

    Add missing cancellationToken

--- Source/EventFlow.EntityFramework/EventStores/EntityFrameworkEventPersistence.cs
+++ Source/EventFlow.EntityFramework/EventStores/EntityFrameworkEventPersistence.cs
@@ -1,19 +1,19 @@
 ﻿// The MIT License (MIT)
-// 
+//
 // Copyright (c) 2015-2018 Rasmus Mikkelsen
 // Copyright (c) 2015-2018 eBay Software Foundation
 // https://github.com/eventflow/EventFlow
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
 // the Software without restriction, including without limitation the rights to
 // use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 // the Software, and to permit persons to whom the Software is furnished to do so,
 // subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 // FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -157,7 +157,7 @@ namespace EventFlow.EntityFramework.EventStores
                     .Set<EventEntity>()
                     .Where(e => e.AggregateId == id.Value
                                 && sequenceNumbers.Contains(e.AggregateSequenceNumber))
-                    .ToListAsync()
+                    .ToListAsync(cancellationToken)
                     .ConfigureAwait(false);
 
                 foreach (var @event in events)
@@ -211,7 +211,7 @@ namespace EventFlow.EntityFramework.EventStores
             {
                 var entities = await context.Set<EventEntity>()
                     .Where(e => e.AggregateId == id.Value)
-                    .Select(e => new EventEntity {GlobalSequenceNumber = e.GlobalSequenceNumber})
+                    .Select(e => new EventEntity { GlobalSequenceNumber = e.GlobalSequenceNumber })
                     .ToListAsync(cancellationToken)
                     .ConfigureAwait(false);
 
