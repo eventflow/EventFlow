0c94d39ff995cde4448c9439c09913bc707cfce9
Rasmus Mikkelsen <r@smus.nu>
2020-07-29 09:17:59 +0200

    Executed Update-FileHeaders.ps1

--- Source/EventFlow.Tests/UnitTests/ReadStores/BaseReadModelTests.cs
+++ Source/EventFlow.Tests/UnitTests/ReadStores/BaseReadModelTests.cs
@@ -1,7 +1,7 @@
 // The MIT License (MIT)
 // 
-// Copyright (c) 2015-2019 Rasmus Mikkelsen
-// Copyright (c) 2015-2019 eBay Software Foundation
+// Copyright (c) 2015-2020 Rasmus Mikkelsen
+// Copyright (c) 2015-2020 eBay Software Foundation
 // https://github.com/eventflow/EventFlow
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
--- Source/EventFlow/Snapshots/SnapshotVersionAttribute.cs
+++ Source/EventFlow/Snapshots/SnapshotVersionAttribute.cs
@@ -1,7 +1,7 @@
-﻿// The MIT License (MIT)
+// The MIT License (MIT)
 // 
-// Copyright (c) 2015 Rasmus Mikkelsen
-// Copyright (c) 2015 eBay Software Foundation
+// Copyright (c) 2015-2020 Rasmus Mikkelsen
+// Copyright (c) 2015-2020 eBay Software Foundation
 // https://github.com/eventflow/EventFlow
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
