ab7e7cf97dbe520fe8e7f8fbaef4dacfa2c10b6e
Devon Herlitz <devon.herlitz@ef.com>
2020-08-25 23:05:48 -0400

    Adding in missing license headers

--- Source/EventFlow.Kafka.Tests/UnitTests/Integrations/KafkaMessageFactoryTests.cs
+++ Source/EventFlow.Kafka.Tests/UnitTests/Integrations/KafkaMessageFactoryTests.cs
@@ -1,20 +1,38 @@
-﻿using AutoFixture;
+﻿// The MIT License (MIT)
+//
+// Copyright (c) 2020 Rasmus Mikkelsen
+// Copyright (c) 2020 eBay Software Foundation
+// https://github.com/rasmus/EventFlow
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using AutoFixture;
 using Confluent.Kafka;
 using EventFlow.Aggregates;
 using EventFlow.EventStores;
-using EventFlow.Extensions;
 using EventFlow.Kafka.Integrations;
 using EventFlow.TestHelpers;
-using EventFlow.TestHelpers.Aggregates;
 using EventFlow.TestHelpers.Aggregates.Events;
-using EventFlow.TestHelpers.Extensions;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace EventFlow.Kafka.Tests.UnitTests.Integrations
 {
