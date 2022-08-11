4906076408074e591f138d55f6f10102e2640cdc
Joshua Hillmann <lycosh211@gmx.de>
2022-08-11 20:12:49 +0200

    teardown and IEventStreamCollectionResolver test

--- EventFlow.Redis.Tests/EventStore/EventStoreTests.cs
+++ EventFlow.Redis.Tests/EventStore/EventStoreTests.cs
@@ -1,9 +1,13 @@
 ﻿using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
+using EventFlow.Core;
 using EventFlow.Extensions;
 using EventFlow.Redis.EventStore;
+using EventFlow.TestHelpers.Aggregates;
+using EventFlow.TestHelpers.Aggregates.ValueObjects;
 using EventFlow.TestHelpers.Suites;
+using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using StackExchange.Redis;
@@ -26,15 +30,39 @@ public class EventStoreTests : TestSuiteForEventStore
         eventFlowOptions.ServiceCollection
             .AddTransient<IEventStreamCollectionResolver, EventStreamCollectionResolver>();
         eventFlowOptions.UseEventPersistence<RedisEventPersistence>();
-        
+
         return base.Configure(eventFlowOptions);
     }
 
     [Test]
-    public void a()
+    public async Task StreamResolverReturnsAllAggregateIds()
     {
-        Console.WriteLine();
+        //Arrange
+        var resolver = ServiceProvider.GetRequiredService<IEventStreamCollectionResolver>();
+        var firstId = ThingyId.New;
+        var secondId = ThingyId.New;
+        var firstAggregate = await LoadAggregateAsync(firstId).ConfigureAwait(false);
+        firstAggregate.Ping(PingId.New);
+        var secondAggregate = await LoadAggregateAsync(secondId).ConfigureAwait(false);
+        secondAggregate.Ping(PingId.New);
+        await firstAggregate.CommitAsync(EventStore, SnapshotStore, SourceId.New, CancellationToken.None).ConfigureAwait(false);
+        await secondAggregate.CommitAsync(EventStore, SnapshotStore, SourceId.New, CancellationToken.None).ConfigureAwait(false);
+
+        //Act
+        var keys = await resolver.GetStreamNamesAsync();
+        var names = keys.Select(k => k.Key);
+
+        //Assert
+        names.Should().Contain(firstId.Value);
+        names.Should().Contain(secondId.Value);
     }
     
+    [Test]
     
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        await _container.DisposeAsync().ConfigureAwait(false);
+    }
 }
