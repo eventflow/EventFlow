09ef7b994f7bcc516c099e60390e11b595b3d9dd
Warren Pieterse <warren.pieterse@gmail.com>
2024-10-22 08:38:49 +0200

    init commit

--- Source/EventFlow.EventStores.EventStore/EventStoreEventPersistence.cs
+++ Source/EventFlow.EventStores.EventStore/EventStoreEventPersistence.cs
@@ -1,4 +1,4 @@
-// The MIT License (MIT)
+﻿// The MIT License (MIT)
 // 
 // Copyright (c) 2015-2024 Rasmus Mikkelsen
 // https://github.com/eventflow/EventFlow
@@ -20,186 +20,196 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+using EventFlow.Aggregates;
+using EventFlow.Core;
+using EventFlow.Exceptions;
+using EventStore.Client;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using EventFlow.Aggregates;
-using EventFlow.Core;
-using EventFlow.Exceptions;
-using EventFlow.Logs;
-using EventStore.ClientAPI;
-using EventStore.ClientAPI.Exceptions;
 
-namespace EventFlow.EventStores.EventStore
+namespace EventFlow.EventStores.EventStore;
+
+public class EventStoreEventPersistence(EventStoreClient eventStoreClient, ILogger<EventStoreEventPersistence> logger) : IEventPersistence
 {
-    public class EventStoreEventPersistence : IEventPersistence
+    private class EventStoreEvent : ICommittedDomainEvent
     {
-        private readonly ILog _log;
-        private readonly IEventStoreConnection _connection;
+        public required string AggregateId { get; set; }
+        public required string Data { get; set; }
+        public required string Metadata { get; set; }
+        public required int AggregateSequenceNumber { get; set; }
+    }
 
-        private class EventStoreEvent : ICommittedDomainEvent
-        {
-            public string AggregateId { get; set; }
-            public string Data { get; set; }
-            public string Metadata { get; set; }
-            public int AggregateSequenceNumber { get; set; }
-        }
-
-        public EventStoreEventPersistence(
-            ILog log,
-            IEventStoreConnection connection)
-        {
-            _log = log;
-            _connection = connection;
-        }
-
-        public async Task<AllCommittedEventsPage> LoadAllCommittedEvents(
-            GlobalPosition globalPosition,
-            int pageSize,
-            CancellationToken cancellationToken)
-        {
-            var nextPosition = ParsePosition(globalPosition);
-            var resolvedEvents = new List<ResolvedEvent>();
-            AllEventsSlice allEventsSlice;
-
-            do
+    public async Task<IReadOnlyCollection<ICommittedDomainEvent>> CommitEventsAsync(IIdentity id, IReadOnlyCollection<SerializedEvent> serializedEvents, CancellationToken cancellationToken)
+    {
+        var committedDomainEvents = serializedEvents
+            .Select(e => new EventStoreEvent
             {
-                allEventsSlice = await _connection.ReadAllEventsForwardAsync(nextPosition, pageSize, false).ConfigureAwait(false);
-                resolvedEvents.AddRange(allEventsSlice.Events.Where(e => !e.OriginalStreamId.StartsWith("$")));
-                nextPosition = allEventsSlice.NextPosition;
+                AggregateSequenceNumber = e.AggregateSequenceNumber,
+                Metadata = e.SerializedMetadata,
+                AggregateId = id.Value,
+                Data = e.SerializedData
+            })
+            .ToList();
 
-            }
-            while (resolvedEvents.Count < pageSize && !allEventsSlice.IsEndOfStream);
+        //var expectedVersion = Math.Max(serializedEvents.Min(e => e.AggregateSequenceNumber) - 2, ExpectedVersion.NoStream);
+        var expectedVersion = serializedEvents.Min(e => e.AggregateSequenceNumber) - 2;
 
-            var eventStoreEvents = Map(resolvedEvents);
+        var eventDatas = serializedEvents
+            .Select(e =>
+                {
+                    // While it might be tempting to use e.Metadata.EventId here, we can't
+                    // as EventStore won't detect optimistic concurrency exceptions then
+                    var uuid = Uuid.NewUuid();
 
-            return new AllCommittedEventsPage(
-                new GlobalPosition(string.Format("{0}-{1}", nextPosition.CommitPosition, nextPosition.PreparePosition)),
-                eventStoreEvents);
-        }
+                    var eventType = string.Format("{0}.{1}.{2}", e.Metadata[MetadataKeys.AggregateName], e.Metadata.EventName, e.Metadata.EventVersion);
+                    var data = Encoding.UTF8.GetBytes(e.SerializedData);
+                    var meta = Encoding.UTF8.GetBytes(e.SerializedMetadata);
+                    return new EventData(uuid, eventType, data, meta);
+                })
+            .ToList();
 
-        private static Position ParsePosition(GlobalPosition globalPosition)
+        try
         {
-            if (globalPosition.IsStart)
+            IWriteResult writeResult;
+            if (expectedVersion < 0)
             {
-                return Position.Start;
-            }
-
-            var parts = globalPosition.Value.Split('-');
-            if (parts.Length != 2)
-            {
-                throw new ArgumentException(string.Format(
-                    "Unknown structure for global position '{0}'. Expected it to be empty or in the form 'L-L'",
-                    globalPosition.Value));
-            }
-
-            var commitPosition = long.Parse(parts[0]);
-            var preparePosition = long.Parse(parts[1]);
-
-            return new Position(commitPosition, preparePosition);
-        }
-
-        public async Task<IReadOnlyCollection<ICommittedDomainEvent>> CommitEventsAsync(
-            IIdentity id,
-            IReadOnlyCollection<SerializedEvent> serializedEvents,
-            CancellationToken cancellationToken)
-        {
-            var committedDomainEvents = serializedEvents
-                .Select(e => new EventStoreEvent
-                    {
-                        AggregateSequenceNumber = e.AggregateSequenceNumber,
-                        Metadata = e.SerializedMetadata,
-                        AggregateId = id.Value,
-                        Data = e.SerializedData
-                    })
-                .ToList();
-
-            var expectedVersion = Math.Max(serializedEvents.Min(e => e.AggregateSequenceNumber) - 2, ExpectedVersion.NoStream);
-            var eventDatas = serializedEvents
-                .Select(e =>
-                    {
-                        // While it might be tempting to use e.Metadata.EventId here, we can't
-                        // as EventStore won't detect optimistic concurrency exceptions then
-                        var guid = Guid.NewGuid();
-
-                        var eventType = string.Format("{0}.{1}.{2}", e.Metadata[MetadataKeys.AggregateName], e.Metadata.EventName, e.Metadata.EventVersion);
-                        var data = Encoding.UTF8.GetBytes(e.SerializedData);
-                        var meta = Encoding.UTF8.GetBytes(e.SerializedMetadata);
-                        return new EventData(guid, eventType, true, data, meta);
-                    })
-                .ToList();
-
-            try
-            {
-                var writeResult = await _connection.AppendToStreamAsync(
+                writeResult = await eventStoreClient.AppendToStreamAsync(
                     id.Value,
-                    expectedVersion,
-                    eventDatas)
-                    .ConfigureAwait(false);
-
-                _log.Verbose(
-                    "Wrote entity {0} with version {1} ({2},{3})",
-                    id,
-                    writeResult.NextExpectedVersion - 1,
-                    writeResult.LogPosition.CommitPosition,
-                    writeResult.LogPosition.PreparePosition);
+                    StreamState.NoStream,
+                    eventDatas,
+                    cancellationToken: cancellationToken).ConfigureAwait(false);
             }
-            catch (WrongExpectedVersionException e)
+            else
             {
-                throw new OptimisticConcurrencyException(e.Message, e);
-            }
-
-            return committedDomainEvents;
-        }
-
-        public async Task<IReadOnlyCollection<ICommittedDomainEvent>> LoadCommittedEventsAsync(
-            IIdentity id,
-            int fromEventSequenceNumber,
-            CancellationToken cancellationToken)
-        {
-            var streamEvents = new List<ResolvedEvent>();
-
-            StreamEventsSlice currentSlice;
-            var nextSliceStart = fromEventSequenceNumber <= 1
-                ? StreamPosition.Start
-                : fromEventSequenceNumber - 1; // Starts from zero
-
-            do
-            {
-                currentSlice = await _connection.ReadStreamEventsForwardAsync(
+                writeResult = await eventStoreClient.AppendToStreamAsync(
                     id.Value,
-                    nextSliceStart,
-                    200,
-                    false)
-                    .ConfigureAwait(false);
-                nextSliceStart = (int)currentSlice.NextEventNumber;
-                streamEvents.AddRange(currentSlice.Events);
-
+                    StreamRevision.FromInt64(expectedVersion),
+                    eventDatas,
+                    cancellationToken: cancellationToken).ConfigureAwait(false);
             }
-            while (!currentSlice.IsEndOfStream);
 
-            return Map(streamEvents);
+            logger.LogDebug(
+                "Wrote entity {0} with version {1} ({2},{3})",
+                id,
+                expectedVersion,
+                writeResult.LogPosition.CommitPosition,
+                writeResult.LogPosition.PreparePosition);
         }
-
-        public Task DeleteEventsAsync(IIdentity id, CancellationToken cancellationToken)
+        catch (WrongExpectedVersionException e)
         {
-            return _connection.DeleteStreamAsync(id.Value, ExpectedVersion.Any);
+            throw new OptimisticConcurrencyException(e.Message, e);
         }
 
-        private static IReadOnlyCollection<EventStoreEvent> Map(IEnumerable<ResolvedEvent> resolvedEvents)
+        return committedDomainEvents;
+    }
+
+    public async Task DeleteEventsAsync(IIdentity id, CancellationToken cancellationToken)
+    {
+        await eventStoreClient.TombstoneAsync(id.Value, StreamState.Any, cancellationToken: cancellationToken);
+    }
+
+    public async Task<AllCommittedEventsPage> LoadAllCommittedEvents(GlobalPosition globalPosition, int pageSize, CancellationToken cancellationToken)
+    {
+        var nextPosition = ParsePosition(globalPosition);
+        var resolvedEvents = new List<ResolvedEvent>();
+
+        await foreach (var resolvedEvent in eventStoreClient.ReadAllAsync(
+            Direction.Forwards, nextPosition, pageSize, cancellationToken: cancellationToken))
         {
-            return resolvedEvents
-                .Select(e => new EventStoreEvent
-                    {
-                        AggregateSequenceNumber = (int)(e.Event.EventNumber + 1), // Starts from zero
-                        Metadata = Encoding.UTF8.GetString(e.Event.Metadata),
-                        AggregateId = e.OriginalStreamId,
-                        Data = Encoding.UTF8.GetString(e.Event.Data),
-                    })
-                .ToList();
+            if (!resolvedEvent.OriginalStreamId.StartsWith("$"))
+            {
+                resolvedEvents.Add(resolvedEvent);
+            }
         }
+
+        var eventStoreEvents = Map(resolvedEvents);
+
+        return new AllCommittedEventsPage(
+            new GlobalPosition($"{resolvedEvents.Last().Event.Position.CommitPosition}-{resolvedEvents.Last().Event.Position.PreparePosition}"),
+            eventStoreEvents);
+    }
+
+    public async Task<IReadOnlyCollection<ICommittedDomainEvent>> LoadCommittedEventsAsync(IIdentity id, int fromEventSequenceNumber, CancellationToken cancellationToken)
+    {
+        return await LoadCommittedEventsAsync(id, fromEventSequenceNumber, 0, cancellationToken);
+    }
+
+    public async Task<IReadOnlyCollection<ICommittedDomainEvent>> LoadCommittedEventsAsync(IIdentity id, int fromEventSequenceNumber, int toEventSequenceNumber, CancellationToken cancellationToken)
+    {
+        var resolvedEvents = new List<ResolvedEvent>();
+
+        var startPosition = fromEventSequenceNumber <= 1
+            ? StreamPosition.Start
+            : StreamPosition.FromInt64(fromEventSequenceNumber - 1);
+
+        try
+        {
+            ReadState result = await eventStoreClient.ReadStreamAsync(
+                Direction.Forwards,
+                id.Value,
+                startPosition,
+                cancellationToken: cancellationToken
+            ).ReadState;
+
+            if (result == ReadState.StreamNotFound)
+            {
+                // No events, the stream doesn't exist i.e. IsNew
+                return [];
+            }
+
+            await foreach (var resolvedEvent in eventStoreClient.ReadStreamAsync(
+                Direction.Forwards,
+                id.Value,
+                startPosition,
+                cancellationToken: cancellationToken).WithCancellation(cancellationToken))
+            {
+                resolvedEvents.Add(resolvedEvent);
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Failed to load committed events for {id.Value}", ex);
+        }
+
+        return Map(resolvedEvents);
+    }
+
+    private static Position ParsePosition(GlobalPosition globalPosition)
+    {
+        if (globalPosition.IsStart)
+        {
+            return Position.Start;
+        }
+
+        var parts = globalPosition.Value.Split('-');
+        if (parts.Length != 2)
+        {
+            throw new ArgumentException(string.Format(
+                "Unknown structure for global position '{0}'. Expected it to be empty or in the form 'L-L'",
+                globalPosition.Value));
+        }
+
+        var commitPosition = ulong.Parse(parts[0]);
+        var preparePosition = ulong.Parse(parts[1]);
+
+        return new Position(commitPosition, preparePosition);
+    }
+
+    private static List<EventStoreEvent> Map(IEnumerable<ResolvedEvent> resolvedEvents)
+    {
+        return resolvedEvents
+            .Select(e => new EventStoreEvent
+            {
+                AggregateSequenceNumber = (int)(e.Event.EventNumber.ToInt64() + 1), // Starts from zero
+                Metadata = Encoding.UTF8.GetString(e.Event.Metadata.ToArray()),
+                AggregateId = e.Event.EventStreamId,
+                Data = Encoding.UTF8.GetString(e.Event.Data.ToArray()),
+            })
+            .ToList();
     }
 }
--- Source/EventFlow.EventStores.EventStore/Extensions/EventFlowOptionsExtensions.cs
+++ Source/EventFlow.EventStores.EventStore/Extensions/EventFlowOptionsExtensions.cs
@@ -20,79 +20,15 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-using EventFlow.Configuration;
-using EventFlow.Core;
 using EventFlow.Extensions;
-using EventStore.ClientAPI;
-using System;
-using System.Net;
 
-namespace EventFlow.EventStores.EventStore.Extensions
+namespace EventFlow.EventStores.EventStore.Extensions;
+
+public static class EventFlowOptionsExtensions
 {
-    public static class EventFlowOptionsExtensions
+    public static IEventFlowOptions UseEventStoreEventStore(this IEventFlowOptions eventFlowOptions)
     {
-        public static IEventFlowOptions UseEventStoreEventStore(
-            this IEventFlowOptions eventFlowOptions)
-        {
-            return eventFlowOptions.UseEventStore<EventStoreEventPersistence>();
-        }
-
-        [Obsolete("Use the overloads with 'uri' parameter instead.")]
-        public static IEventFlowOptions UseEventStoreEventStore(
-            this IEventFlowOptions eventFlowOptions,
-            IPEndPoint ipEndPoint)
-        {
-            return eventFlowOptions
-                .UseEventStoreEventStore(ipEndPoint, ConnectionSettings.Default);
-        }
-
-        [Obsolete("Use the overloads with 'uri' parameter instead.")]
-        public static IEventFlowOptions UseEventStoreEventStore(
-            this IEventFlowOptions eventFlowOptions,
-            IPEndPoint ipEndPoint,
-            ConnectionSettings connectionSettings)
-        {
-            var eventStoreConnection = EventStoreConnection.Create(
-                connectionSettings,
-                ipEndPoint,
-                $"EventFlow v{typeof(EventFlowOptionsExtensions).Assembly.GetName().Version}");
-
-            using (var a = AsyncHelper.Wait)
-            {
-                a.Run(eventStoreConnection.ConnectAsync());
-            }
-
-            return eventFlowOptions
-                .RegisterServices(f => f.Register(r => eventStoreConnection, Lifetime.Singleton))
-                .UseEventStore<EventStoreEventPersistence>();
-        }
-
-        public static IEventFlowOptions UseEventStoreEventStore(
-            this IEventFlowOptions eventFlowOptions,
-            Uri uri,
-            ConnectionSettings connectionSettings,
-            string connectionNamePrefix = null)
-        {
-            var sanitizedConnectionNamePrefix = string.IsNullOrEmpty(connectionNamePrefix)
-                ? string.Empty
-                : connectionNamePrefix + " - ";
-
-            var eventStoreConnection = EventStoreConnection.Create(
-                connectionSettings,
-                uri,
-                $"{sanitizedConnectionNamePrefix}EventFlow v{typeof(EventFlowOptionsExtensions).Assembly.GetName().Version}");
-
-#pragma warning disable 618
-            // TODO: Figure out bootstrapping alternative for 1.0
-            using (var a = AsyncHelper.Wait)
-            {
-                a.Run(eventStoreConnection.ConnectAsync());
-            }
-#pragma warning restore 618
-
-            return eventFlowOptions
-                .RegisterServices(f => f.Register(r => eventStoreConnection, Lifetime.Singleton))
-                .UseEventStore<EventStoreEventPersistence>();
-        }
+        return eventFlowOptions
+            .UseEventPersistence<EventStoreEventPersistence>();
     }
 }
