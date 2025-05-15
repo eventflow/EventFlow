1a32b4b69d92e22343685a3cd79ff17048e6c4df
AlexGreatDev <AlexanderGreatDev@gmail.com>
2025-05-15 20:38:10 +0330

    Add EventFlowMongoInitializer for MongoDB setup
    Introduces the `EventFlowMongoInitializer` class in the
    `EventFlow.MongoDB.EventStore` namespace. This class
    facilitates the initialization of MongoDB event persistence
    by utilizing an `IServiceProvider` to manage dependencies
    and service lifetimes. The `Initialize` method creates a
    scope and retrieves an instance of `IMongoDbEventPersistenceInitializer`
    to perform the necessary setup.

---
+++ Source/EventFlow.MongoDB/EventStore/EventFlowMongoInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventFlow.MongoDB.EventStore
+{
+    public class EventFlowMongoInitializer
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public EventFlowMongoInitializer(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Initialize()
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var initializer = scope.ServiceProvider.GetRequiredService<IMongoDbEventPersistenceInitializer>();
+            initializer.Initialize();
+        }
+    }
+}
